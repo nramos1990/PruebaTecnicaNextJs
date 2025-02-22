8851e8c8c8ed03df44e112b1a06cdea3fd4cab3d
nramos1990 <nicolas.ramos.ibarra@gmail.com>
2025-02-21 21:25:07 -0300

    WIP: Dashboard design, routes, etc

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Montserrat_Alternates, Lusitana } from 'next/font/google'
+
+const inter = Montserrat_Alternates({ weight: '400' })
+const lusitana = Lusitana({ weight: '400' })
+
+export { inter, lusitana }
