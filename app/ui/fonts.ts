75a8915456ae901c2be94e2676938854936d04e1
nramos1990 <nicolas.ramos.ibarra@gmail.com>
2025-02-21 21:43:36 -0300

    Add subsets to fonts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,6 +1,6 @@
 import { Montserrat_Alternates, Lusitana } from 'next/font/google'
 
-const inter = Montserrat_Alternates({ weight: '400' })
-const lusitana = Lusitana({ weight: '400' })
+const inter = Montserrat_Alternates({ weight: '400', subsets: ['latin'] })
+const lusitana = Lusitana({ weight: '400', subsets: ['latin'] })
 
 export { inter, lusitana }
